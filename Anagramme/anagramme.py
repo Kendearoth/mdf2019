024b8186ffe97aef5e75b3f80b2600d8cfa3f073
Corentin Marie <cmarie@oui.sncf>
2019-09-23 17:37:58 +0200

    init

---
+++ Anagramme/anagramme.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+class Executor:
+
+    @staticmethod
+    def run(inputs):
+        # code here
+        print("test")
+        return ["We", "❤", "python"]
+
+Executor.run(["test"])
