9e852c2dd1c83e9d558a28e14557a3b53fa61cc6
Corentin Marie <cmarie@oui.sncf>
2019-09-26 10:35:45 +0200

    ajout gitignore + anagramme

--- Anagramme/anagramme.py
+++ Anagramme/anagramme.py
@@ -3,8 +3,22 @@ class Executor:
 
     @staticmethod
     def run(inputs):
-        # code here
-        print("test")
-        return ["We", "❤", "python"]
+        print(inputs)
+        results = []
+        base = inputs[0]
 
-Executor.run(["test"])
+        isAnagram = True
+        for word in inputs[1:]:
+            cp = list(word)
+            for charB in base:
+                if charB in cp:
+                    cp.remove(charB)
+                else:
+                    isAnagram = False
+                    break
+            if cp == [] and base != word and len(base) == len(word):
+                results.append(word)
+        print(results)
+        return results
+
+Executor.run(["test", "estt"])
