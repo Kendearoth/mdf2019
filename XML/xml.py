1e15e255864748fa1ed5863817937062dc0fda05
Corentin Marie <cmarie@oui.sncf>
2019-10-01 14:25:42 +0200

    exo concours blanc XML

--- XML/xml.py
+++ XML/xml.py
@@ -12,21 +12,45 @@ lines = []
 listChar = []
 for line in sys.stdin:
     sys.stderr.write(str(line))
-    res = []
+    #calcul de profondeur
+    profondeur = []
     p = 0
     i = 0
     while i < len(line):
         if line[i] != "-" and line[i] != "\n":
             p+=1
-            res.append([line[i],p])
+            profondeur.append([line[i],p])
             i+=1
         elif line[i] == "-":
             p-=1
             i+=2
         else:
             i+=1
-    res.sort(key=takeSecond)
-    sys.stderr.write(str(res))
 
+    #calcul du poids
+    poids = [[],[]]
+    for elem in  profondeur:
+        if elem[0] in poids[0]:
+            i = poids[0].index(elem[0])
+            value = poids[1][i]
+            poids[1][i] = value + 1/elem[1]
+        else:
+            poids[0].append(elem[0])
+            poids[1].append(1/elem[1])
 
-#lines.append(line.rstrip('\n'))
+    #lettre max par ordre alphabétique
+    maxLetter = ""
+    maxValue = 0
+    n = 0
+    while n < len(poids[1]):
+        if poids[1][n] > maxValue:
+            maxValue = poids[1][n]
+            maxLetter = poids[0][n]
+        elif poids[1][n] == maxValue:
+            if poids[0][n] < maxLetter:
+                maxLetter = poids[0][n]
+        n += 1
+    sys.stderr.write(maxLetter)
+    sys.stderr.write(str(maxValue))
+
+    print(maxLetter)
